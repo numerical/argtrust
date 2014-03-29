0056a67b35349745519673059f3c84e0c759fefc
numeral <numerical@gmail.com>
2014-03-29 18:38:22 -0400

    Add some tests

--- tests/test_framework.py
+++ tests/test_framework.py
@@ -1,8 +1,10 @@
-from unittest import TestCase
+import unittest
+from argtrust.framework import ArgumentationFramework
 
-class TestArgumentationFramework(TestCase):
+class TestArgumentationFramework(unittest.TestCase):
 
     def setUp(self):
+        """See Semantics_Overview.pdf for these figures"""
         # Empty
         self.fig0 = ArgumentationFramework( {}, {} )
 
@@ -14,10 +16,10 @@ class TestArgumentationFramework(TestCase):
                 { ('A', 'B'), ('B', 'C'), ('C', 'A') })
 
         # Nixon diamond
-        self.fig3 = ArgumentaitonFrameWork( {'A', 'B'},
+        self.fig3 = ArgumentationFramework( {'A', 'B'},
                 { ('A', 'B'), ('B', 'A') } )
 
-        self.fig4 = ArgumentaitonFrameWork( {'A', 'B', 'C', 'D'},
+        self.fig4 = ArgumentationFramework( {'A', 'B', 'C', 'D'},
                 { ('A', 'B'), ('B', 'C'), ('C', 'B'), ('C', 'D') } )
 
         self.fig5 = ArgumentationFramework( {'A', 'B', 'C', 'D', 'E'},
@@ -25,4 +27,30 @@ class TestArgumentationFramework(TestCase):
 
         self.fig6 = ArgumentationFramework( {'A', 'B', 'C', 'D'},
                 { ('A', 'A'), ('A', 'C'), ('B', 'C'), ('C', 'D') })
+
+    def test_minus(self):
+        self.assertEqual(self.fig1.minus('A'), {'B'})
+        self.assertEqual(self.fig1.minus('B'), {'C'})
+        self.assertEqual(self.fig1.minus('C'), set())
+
+    def test_plus(self):
+        self.assertEqual(self.fig1.plus('A'), set())
+        self.assertEqual(self.fig1.plus('B'), {'A'})
+        self.assertEqual(self.fig1.plus('C'), {'B'})
+
+    def test_args_plus(self):
+        pass
+
+    def test_args_minus(self):
+        pass
+
+    def test_defends(self):
+        pass
+
+    def test_F(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
 
