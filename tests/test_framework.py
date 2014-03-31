6413c53daa798b0b80d2c363aca07c34545d877a
numeral <numerical@gmail.com>
2014-03-30 21:06:15 -0400

    More tests.

--- tests/test_framework.py
+++ tests/test_framework.py
@@ -29,7 +29,12 @@ class TestArgumentationFramework(unittest.TestCase):
                 { ('A', 'A'), ('A', 'C'), ('B', 'C'), ('C', 'D') })
 
     def test_make_generator(self):
-        raise NotImplementedError
+        generator = self.fig6.make_generator()
+        count = 0
+        for s in generator:
+            count += 1
+            self.assertTrue(s.issubset(self.fig6._Ar))
+        self.assertEqual(count, 2**len(self.fig6))
 
     def test_minus(self):
         self.assertEqual(self.fig1.minus('A'), {'B'})
@@ -55,17 +60,47 @@ class TestArgumentationFramework(unittest.TestCase):
         self.assertEqual(self.fig6.args_minus({'A', 'B', 'C', 'D'}), {'A', 'C', 'B'})
 
     def test_conflict_free(self):
-        raise NotImplementedError
+        self.assertTrue(self.fig0.conflict_free(set()))
+        self.assertTrue(self.fig1.conflict_free({'A','C'}))
+        self.assertTrue(self.fig1.conflict_free({'A'}))
+        self.assertTrue(self.fig3.conflict_free({'A'}))
+        self.assertTrue(self.fig3.conflict_free({'B'}))
+        self.assertTrue(self.fig5.conflict_free({'A', 'C', 'E'}))
+
+        self.assertFalse(self.fig1.conflict_free({'B', 'A'}))
+        self.assertFalse(self.fig1.conflict_free({'A', 'B', 'C'}))
+        self.assertFalse(self.fig2.conflict_free({'C', 'B'}))
+        self.assertFalse(self.fig6.conflict_free({'A', 'A'}))
 
     def test_defends(self):
-        raise NotImplementedError
+        self.assertTrue(self.fig1.defends({'C'}, 'A'))
+        self.assertTrue(self.fig2.defends({'C'}, 'B'))
+        self.assertTrue(self.fig4.defends({'A', 'C', 'B'}, 'D'))
+        self.assertTrue(self.fig6.defends({'A', 'B'}, 'D'))
+
+        self.assertFalse(self.fig1.defends({'B'}, 'A'))
+        self.assertFalse(self.fig4.defends({'C', 'D'}, 'B'))
+        self.assertFalse(self.fig6.defends({'A'}, 'C'))
 
     def test_F(self):
-        raise NotImplementedError
+        self.assertEqual(self.fig1.F({'C', 'A'}), {'A', 'C'})
+        self.assertEqual(self.fig1.F({'C'}), {'A', 'C'})
+        self.assertEqual(self.fig2.F({'A'}), {'C'})
+        self.assertEqual(self.fig3.F({'A'}), {'A'})
+        self.assertEqual(self.fig3.F({'B'}), {'B'})
+        self.assertEqual(self.fig4.F({'B'}), {'A', 'D'})
+        self.assertEqual(self.fig5.F({'C', 'D'}), {'A', 'E', 'D'})
 
     def test_admissible(self):
-        raise NotImplementedError
+        self.assertTrue(self.fig1.admissible({'C'}))
+        self.assertTrue(self.fig1.admissible({'A', 'C'}))
+        self.assertTrue(self.fig3.admissible({'B'}))
+        self.assertTrue(self.fig3.admissible({'A'}))
+        self.assertTrue(self.fig4.admissible({'A', 'C'}))
 
+        self.assertFalse(self.fig2.admissible({'A'}))
+        self.assertFalse(self.fig2.admissible({'C'}))
+        self.assertFalse(self.fig6.admissible({'A', 'B', 'D'}))
 
 if __name__ == "__main__":
     unittest.main()
