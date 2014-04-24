8db0454648262733734a23c0e26998922f764551
numeral <numerical@gmail.com>
2014-04-25 00:05:00 +0100

    Add namedtuples for Belief, Rule, and Predicate

--- __init__.py
+++ __init__.py
@@ -1,3 +1,9 @@
-
+from collections import namedtuple as _namedtuple
 class BadImplementationError(Exception):
     pass
+
+Predicate = _namedtuple('Predicate', ['predicate', 'true'])
+Rule = _namedtuple('Rule', ['predicates', 'conclusions'])
+Belief = _namedtuple('Belief', ['agent', 'predicate', 'level'])
+
+
