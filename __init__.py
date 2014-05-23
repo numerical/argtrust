0c77b6f4c43d9b6c9cca704c10dfa5faa8ab92f0
numeral <numerical@gmail.com>
2014-05-23 15:33:55 +0100

    Add namedtuples for Trust, Labelling, Belief, and Attack

--- __init__.py
+++ __init__.py
@@ -2,8 +2,12 @@ from collections import namedtuple as _namedtuple
 class BadImplementationError(Exception):
     pass
 
+# Types
+Trust = _namedtuple('Trust', ['truster', 'trusted'])
 Predicate = _namedtuple('Predicate', ['predicate', 'true'])
 Rule = _namedtuple('Rule', ['predicates', 'conclusions'])
 Belief = _namedtuple('Belief', ['agent', 'predicate', 'level'])
-
+Argument = _namedtuple('Argument', ['arg', 'belief'])
+Labelling = _namedtuple('Labelling', ['inside', 'outside', 'undecided'])
+Attack = _namedtuple('Attack', ['attacker', 'attacked'])
 
