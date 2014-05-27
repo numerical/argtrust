fab84035bdc73c5323963aec696d675c2b1aabf5
numeral <numerical@gmail.com>
2014-05-27 18:28:48 +0100

    Change Argument tuple

--- __init__.py
+++ __init__.py
@@ -7,7 +7,7 @@ Trust = _namedtuple('Trust', ['truster', 'trusted'])
 Predicate = _namedtuple('Predicate', ['predicate', 'true'])
 Rule = _namedtuple('Rule', ['predicates', 'conclusions'])
 Belief = _namedtuple('Belief', ['agent', 'predicate', 'level'])
-Argument = _namedtuple('Argument', ['arg', 'belief'])
+Argument = _namedtuple('Argument', ['predicates', 'conclusion'])
 Labelling = _namedtuple('Labelling', ['inside', 'outside', 'undecided'])
 Attack = _namedtuple('Attack', ['attacker', 'attacked'])
 
